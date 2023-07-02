d5fa34aa6c51309beee6cbd2e178b9d1a89382e4
이준원 <jnwn@jnwn-MacBook-Air.local>
2023-07-02 18:54:43 +0900

    google verify ownership

--- src/pages/404.js
+++ src/pages/404.js
@@ -28,6 +28,7 @@ const page404 = () => {
                 <meta name="googlebot" content="index, follow" />
                 <meta name="google" content="notranslate" />
                 <meta name="format-detection" content="telephone=no" />
+                <meta name="google-site-verification" content="G6-aAhPgTXHhI-3wYIV_mFVFuOUsr_zI8iwHPP_Gv4U" />
             </MetaTags>
 
             {/* <Loading /> */}
